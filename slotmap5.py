139db9b61ae81a3bed5bb6f6e2cb15a6c4364c50
Pete Hartman <pete.hartman@nexenta.com>
2019-06-13 14:38:05 -0500

    call out LEGACY_SAS devices not shown in any enclosure

--- slotmap5.py
+++ slotmap5.py
@@ -12,8 +12,11 @@
 # pwh - 05/15/2018 changed to use name "disk.previouslyDiscovered"
 #   rather than description "List of previously discovered disks"
 # pwh - 08/14/2018 fixed typo of "bayid" to "bayId" when slot is empty
+# pwh - 06/13/2019 fix to deal with situation where some enclosures are labelled and others are not
+#                  also call out drives that show up as "LEGACY_SAS" instead of in an enclosure
 #
 import json
+from operator import itemgetter
 
 valid=0
 
@@ -23,7 +26,7 @@ with open('sysconfig.json') as json_data:
     
 enclosureNames = {}
 diskDetails = {}
-
+unmappedDisks = {}
 
 # older sysconfig.json files appear to omit the disk list
 #
@@ -47,12 +50,16 @@ if valid:
             enclosureInfo = item['currentValue']
         elif item['name'] == "disk.previouslyDiscovered":
             diskInfo = item['currentValue']
-
+    
     for disk in diskInfo:
         diskDetails[disk['devname']] = disk
+        if (disk['enclosure'] == 'LEGACY_SAS'):
+            unmappedDisks[disk['devname']] = disk
 
     for encl in enclosureInfo:
-        if enclosureNames and enclosureNames[encl['chassisId']] != "RETIRED":
+        # it is possible for us to have an un-retired but not labelled chassis, even when other chassis are labelled
+        # ensure that encl['chassisId'] really shows up in the label list before referencing it so we don't blow up
+        if enclosureNames and (encl['chassisId'] in enclosureNames) and enclosureNames[encl['chassisId']] != "RETIRED":
             print "Enclosure:", enclosureNames[encl['chassisId']], encl['chassis'][0]['vendorId'], encl['chassis'][0]['productId'], encl['chassisId'] 
             for bay in encl['bays']:
                 if bay['empty']:
@@ -62,7 +69,7 @@ if valid:
                     capacityGiB = int(rawCapacity/(1024 * 1024 * 1024))
                     print "\t%2d" % bay['bayId'], "\t", bay['description'], "\t", diskDetails[bay['devname']]['logicalDevice'], "\t", diskDetails[bay['devname']]['vendorId'], diskDetails[bay['devname']]['productId'], "\t%5d GiB" % capacityGiB, "\t", bay['devname']
             print
-        elif not enclosureNames:
+        elif not enclosureNames or not (encl['chassisId'] in enclosureNames):
             print "Enclosure: (no label)", encl['chassis'][0]['vendorId'], encl['chassis'][0]['productId'], encl['chassisId'] 
             for bay in encl['bays']:
                 if bay['empty']:
@@ -71,7 +78,12 @@ if valid:
                     rawCapacity = diskDetails[bay['devname']]['capacity']
                     capacityGiB = int(rawCapacity/(1024 * 1024 * 1024))
                     print "\t%2d" % bay['bayId'], "\t", bay['description'], "\t", diskDetails[bay['devname']]['logicalDevice'], "\t", diskDetails[bay['devname']]['vendorId'], diskDetails[bay['devname']]['productId'], "\t%5d GiB" % capacityGiB, "\t", bay['devname']
-            print
+    if unmappedDisks:
+        print "\nThe following devices are not mapped to any enclosure (they show up as 'LEGACY_SAS'):\n"
+        for disk in sorted(unmappedDisks.values(), key=itemgetter('bay')):
+            rawCapacity = disk['capacity']
+            capacityGiB = int(rawCapacity/(1024 * 1024 * 1024))
+            print "\t%2d" % disk['bay'], "\t\t\t", disk['logicalDevice'], "\t", disk['vendorId'], disk['productId'], "\t%5d GiB" % capacityGiB, "\t", disk['devname']
 else:
     print "Could not find disk list in this sysconfig.json file"
     exit 
